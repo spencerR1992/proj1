9cd7480333d4874cb52de0a5b20598b06faac2d2
Spencer Romo <spencer.r.romo@gmail.com>
2016-05-29 14:33:27 -0500

    Adding in Sector and Industry models.

--- computation/proj1/special/models.py
+++ computation/proj1/special/models.py
@@ -10,5 +10,17 @@ class Company(models.Model):
 	name = models.CharField(max_length=300)
 	ticker_symbol = models.CharField(max_length=20,unique=True)
 	stock_data = JSONField(null=True,blank=True)
+	sector = models.CharField(max_length=200,blank=True,null=True)
+	industry = models.CharField(max_length=200,blank=True,null=True)
 	def __unicode__(self):
 		return self.ticker_symbol
+
+class Industry(models.Model):
+	name = models.CharField(max_length=300,unique=True)
+	def __unicode__(self):
+		return self.name
+
+class Sector(models.Model):
+	name = models.CharField(max_length=500,unique=True)
+	def __unicode__(self):
+		return self.name
--- computation/proj1/utils.py
+++ computation/proj1/utils.py
@@ -2,22 +2,35 @@ import json
 import random
 from special.models import Company
 
+
 def unpack_json_data(datafile):
-	try:
-		with open(datafile) as data_file:
-			myData=json.load(data_file)
-		return myData
-	except Exception, e:
-		print str(e)
+    try:
+        with open(datafile) as data_file:
+            myData = json.load(data_file)
+        return myData
+    except Exception, e:
+        print str(e)
 
 
-def create_company(stock_name,ticker_symbol):
-	try:
-		a = Company(name=stock_name,ticker_symbol=ticker_symbol)
-		a.save()
-	except Exception, e:
-		print str(e)
+def create_company(stock_name, ticker_symbol):
+    try:
+        a = Company(name=stock_name, ticker_symbol=ticker_symbol)
+        a.save()
+    except Exception, e:
+        print str(e)
 
+#needs to be tested
+#this unpacks all company information from the json files provided.
+def company_info_to_dict():
+    nyse = 'nyse.json'
+    nasdaq = 'nasdaq.json'
+    amex = 'amex.json'
+    myList = [nyse, nasdaq, amex]
+    myDict = {}
+    for item in myList:
+        myData = unpack_json_data(item)
+        for item in myData:
+            myDict[item['Symbol']] = {'name': item['Name'], 'last_sale': item['LastSale'], 'market_cap': item['MarketCap'], 'ipo_year': item[
+                'IPOyear'], 'sector': item['Sector'], 'industry': item['industry'], 'summary_quote': item['Summary Quote']}
 
-
-	
+    return myDict
